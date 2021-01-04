c3782fcb54a3b454231718c5c21e7ef40954266f
camelia84 <57041859+camelia84@users.noreply.github.com>
2021-01-04 12:26:49 +0100

    rename service and controller

--- ApiDomain/src/main/java/com/maranata/api/domain/service/PersonaService.java
+++ ApiDomain/src/main/java/com/maranata/api/domain/service/PersonaService.java
@@ -3,9 +3,11 @@ package com.maranata.api.domain.service;
 import com.maranata.api.domain.dao.PersonaRepository;
 import com.maranata.api.domain.entity.Persona;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class PersonaService {
 
     @Autowired
