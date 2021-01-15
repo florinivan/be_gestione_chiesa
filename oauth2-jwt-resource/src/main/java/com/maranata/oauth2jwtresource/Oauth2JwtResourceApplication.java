6edc6c3f9d43e6d84d1a4e01e80fef3b3a6d27ee
camelia84 <57041859+camelia84@users.noreply.github.com>
2021-01-15 10:25:36 +0100

    Initial commit

---
+++ oauth2-jwt-resource/src/main/java/com/maranata/oauth2jwtresource/Oauth2JwtResourceApplication.java
@@ -0,0 +1,13 @@
+package com.maranata.oauth2jwtresource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2JwtResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2JwtResourceApplication.class, args);
+	}
+
+}
---
+++ oauth2-jwt-resource/src/main/java/com/maranata/oauth2jwtresource/config/WebSecurityConfiguration.java
@@ -0,0 +1,8 @@
+package com.maranata.oauth2jwtresource.config;
+
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
+
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfiguration {
+}
