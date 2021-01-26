e16cf77cf71d2024ba8530d23c696c898d736df8
camelia84 <57041859+camelia84@users.noreply.github.com>
2021-01-26 15:18:29 +0100

    OAuth2 security microservices

---
+++ auth-server/src/test/java/com/maranata/authserver/AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maranata.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
