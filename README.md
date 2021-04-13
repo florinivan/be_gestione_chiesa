6e10a861c2b9030c88bf56aeb4bafe32edeea34f
Eduard <edu@MBP-di-Eduard.homenet.telecomitalia.it>
2021-04-13 12:09:06 +0200

    fixed naming convention of all projects

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # be_gestione_chiesa
 API per la gestione della chiesa
+
+
+SERVICES PORT LIST
+
+ApiManagement               ->  8081 
+DomainManagement        ->  8082
+
+DomainBudgetPlanner     ->  8084
+ApiBudgetPlanner            ->  8085
--- oauth2-jwt-resource/src/main/java/com/maranata/oauth2jwtresource/config/WebSecurityConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.maranata.oauth2jwtresource.config;
-
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-
-
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class
-WebSecurityConfiguration {
-}
